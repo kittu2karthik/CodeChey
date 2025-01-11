0f99b52668ec5aee4e14277f0102165c50ef494e
kittu2karthik <kittu2karthik@gmail.com>
2025-01-11 23:52:54 +0530

    fixed tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
@@ -53,5 +53,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar")],
+  plugins: [import("tailwindcss-animate"), import("tailwind-scrollbar")],
 };
